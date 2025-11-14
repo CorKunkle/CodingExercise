ef724d5142ce3df33fc23aeb0b71b7e728248a45
Corey Kunkle <cor.kunkle@gmail.com>
2025-11-14 14:24:21 -0500

    feat: Added API controller and tests

---
+++ InvestmentPerformance.Tests/InvestmentAPITests.cs
@@ -0,0 +1,54 @@
+﻿using InvestmentPerformance.Api.Controllers;
+using InvestmentPerformance.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace InvestmentPerformance.Tests;
+
+public class InvestmentApiTests
+{
+    private const int ValidUserId = 101;
+    private const int ValidInvestmentId = 1;
+    private const int InvalidInvestmentId = -1;
+    
+    [Fact]
+    public void GetUserInvestments_GivenValidUser_ReturnsOk()
+    {
+        var investmentService = new Mock<InvestmentService>();
+        var logger = new Mock<ILogger<InvestmentController>>();
+        var investmentController = new InvestmentController(investmentService.Object, logger.Object);
+        
+        var result = investmentController.GetUserInvestments(ValidUserId.ToString());
+        
+        Assert.IsType<OkObjectResult>(result.Result);
+        
+    }
+
+    [Fact]
+    public void GetInvestment_GivenValidInvestment_ReturnsOk()
+    {
+        var investmentService = new Mock<InvestmentService>();
+        var logger = new Mock<ILogger<InvestmentController>>();
+        var investmentController = new InvestmentController(investmentService.Object, logger.Object);
+        
+        var result = investmentController.GetInvestment(ValidInvestmentId.ToString());
+        
+        Assert.IsType<OkObjectResult>(result);
+        
+    }
+
+    [Fact]
+    public void GetInvestment_GivenInvalidInvestment_ReturnsNotFound()
+    {
+        var investmentService = new Mock<InvestmentService>();
+        var logger = new Mock<ILogger<InvestmentController>>();
+        var investmentController = new InvestmentController(investmentService.Object, logger.Object);
+        
+        var result = investmentController.GetInvestment(InvalidInvestmentId.ToString());
+        
+        Assert.IsType<NotFoundResult>(result);
+    }
+    
+}
---
+++ InvestmentPerformance/src/Controllers/InvestmentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using InvestmentPerformance.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InvestmentPerformance.Api.Controllers;
+
+[ApiController]
+[Route("investments")]
+public sealed class InvestmentController(IInvestmentService investmentService, ILogger<InvestmentController> logger)
+    : ControllerBase
+{
+    /// <summary>
+    /// Retrieves a list of investments for a given user
+    /// </summary>
+    /// <param name="id">Id of the user you want investment data for</param>
+    /// <returns>List of UserInvestment record for the user</returns>
+    [HttpGet("users/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<UserInvestmentRecord>> GetUserInvestments(string id)
+    {
+        try
+        {
+            var result = investmentService.GetInvestmentsByUserId(int.Parse(id));
+
+            return Ok(result);
+        }
+        catch(Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            return BadRequest();
+        }
+    }
+
+    /// <summary>
+    /// Retrieves the information of a given investment
+    /// </summary>
+    /// <param name="id">Id of the investment you want information on</param>
+    /// <returns>Investment record for the given investment</returns>
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetInvestment(string id)
+    {
+        try
+        {
+            var result = investmentService.GetInvestmentById(int.Parse(id));
+
+            if(result == null)
+                return NotFound();
+
+        
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            return BadRequest();
+        }
+
+    }
+}
